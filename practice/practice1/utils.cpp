ad55e0a875657a510aa03448368f0d9e781b57c8
Luca Novello <luca@lucanovello.com>
2024-07-30 00:54:23 -0400

    updated practice

--- practice/practice1/utils.cpp
+++ practice/practice1/utils.cpp
@@ -1,7 +1,3 @@
 #include <iostream>
 #include "utils.h"
 
-int main() {
-
-	return 0;
-}
---
+++ practice/practice1/utils.h
@@ -0,0 +1,8 @@
+#ifndef PRACTICE_UTILS_H
+#define PRACTICE_UTILS_H
+#include <iostream>
+
+
+
+#endif // !PRACTICE_UTILS_H3
+
