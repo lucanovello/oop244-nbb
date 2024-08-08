50b7db0f1dad5af191be97854e26579a7207b899
Luca Novello <luca@lucanovello.com>
2024-08-08 12:10:04 -0400

    added ms3

---
+++ milestones/ms3/Lib.h
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////////
+// Final Project Milestone
+// Date Module
+// File:    Date.h
+// Version: 1.0
+// Author:	Luca Novello
+/////////////////////////////////////////////////////////////////
+#ifndef SENECA_LIB_H__
+#define SENECA_LIB_H__
+#include <iostream>
+
+namespace seneca {
+
+
+		
+}	
+#endif // !SENECA_LIB_H__
